209f8660cf4cc5e4ed78b089e1a340ff2a6ae472
ravindu644 <droidcasts@protonmail.com>
2025-10-10 00:52:31 +0530

    BoardConfig: set the default frame rate to 90Hz
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,7 @@ PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
+TW_FRAMERATE := 90
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
