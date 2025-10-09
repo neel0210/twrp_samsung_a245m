e8348067bc2efc36f3084bc4179c921b6bf2ffa7
ravindu644 <droidcasts@protonmail.com>
2025-10-09 22:40:32 +0530

    Initial Device tree Generation from twrpdtgen
    https://github.com/twrpdtgen/twrpdtgen
    
    ---
    pip3 install twrpdtgen
    python3 -m twrpdtgen recovery.img
    ---
    
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

---
+++ omni_a16xm.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a16xm device
+$(call inherit-product, device/samsung/a16xm/device.mk)
+
+PRODUCT_DEVICE := a16xm
+PRODUCT_NAME := omni_a16xm
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A166P
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a16xmnsxx-user 13 TP1A.220624.014 A166PXXS5BYG2 release-keys"
+
+BUILD_FINGERPRINT := samsung/a16xmnsxx/a16xm:13/TP1A.220624.014/A166PXXS5BYG2:user/release-keys
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_a16xm-user
+add_lunch_combo omni_a16xm-userdebug
+add_lunch_combo omni_a16xm-eng
